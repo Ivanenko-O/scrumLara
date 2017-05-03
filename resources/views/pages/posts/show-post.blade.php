87133fda8e575a2223a45389791bce83308156f0
olesia66 <lesia@tridentsoftlab.com>
2017-05-03 17:59:52 +0300

    fixed show-post

--- resources/views/pages/posts/show-post.blade.php
+++ resources/views/pages/posts/show-post.blade.php
@@ -1,8 +1,40 @@
 @extends('index')
 
 @section('content')
-<h1> {{ $post->title }}</h1>
 
-<?php echo $post->body; ?>
+    {{--Page Title --}}
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1> {{ $post->title }} </h1>
+            <ol class="breadcrumb">
+                <li><a href="">Главная</a></li>
+                <li><a href="blog">Блог</a></li>
+                <li class="active">Статья</li>
+            </ol>
+        </div>
+    </section>
+    {{-- #page-title end --}}
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content -->
+                <div class="postcontent nobottommargin clearfix">
+
+                    <div class="single-post nobottommargin">
+
+                        <!-- Single Post -->
+                        <div class="entry clearfix">
+
+                            <?php echo $post->body; ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
